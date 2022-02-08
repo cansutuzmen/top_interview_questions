f1a73d5f77fe229dbed8bbc4eac6da9fae22b068
cansutuzmen <52562509+cansutuzmen@users.noreply.github.com>
2022-02-08 11:18:27 +0300

    Create fizzBuzz.js

---
+++ fizzBuzz.js
@@ -0,0 +1,30 @@
+//LeetCode - Fizz buzz
+/*
+https://leetcode.com/problems/fizz-buzz/
+*/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    let result = [];
+    
+    for(let i = 1; i <= n; i++){
+        if(i % 5 === 0 && i % 3 === 0){
+            result.push("FizzBuzz");
+        }
+        else if(i % 3 === 0){
+            result.push("Fizz");
+        }
+        else if(i % 5 === 0){
+            result.push("Buzz");
+        }else{
+           result.push(`${i}`);
+        }
+    }
+      
+    return result;
+};
+
+
