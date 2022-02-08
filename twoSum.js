ff37403618ed96836a525bfd192bae178dd1d79b
cansutuzmen <52562509+cansutuzmen@users.noreply.github.com>
2022-02-08 11:04:27 +0300

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,27 @@
+//leetcode - two sum
+/*
+https://leetcode.com/problems/two-sum/
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    if(!nums.length){
+        return [];
+    }
+    
+    const map = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        const difference = target - nums[i];
+
+        if (difference in map) {
+            return [map[difference], i]
+        }
+
+        map[nums[i]] = i
+    }
+};
