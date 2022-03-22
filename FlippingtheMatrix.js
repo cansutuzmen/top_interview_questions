6dfd1526f548de8ad2e8260e8b5a5ca0e345b08e
Cansu Tuzmen <52562509+cansutuzmen@users.noreply.github.com>
2022-03-22 12:48:00 +0300

    Create FlippingtheMatrix.js

---
+++ FlippingtheMatrix.js
@@ -0,0 +1,26 @@
+/**
+Hackerrank - Flipping the Matrix
+https://www.hackerrank.com/challenges/flipping-the-matrix/problem
+**/
+
+function flippingMatrix(matrix) {
+    let sum = 0;
+    let quadrantSize = matrix.length/2;
+  
+    for(let i = 0; i < quadrantSize; i++){
+      for(let j = 0; j < quadrantSize; j++){
+        const q1 = matrix[i][(2 * quadrantSize) - j - 1)];
+        const q2 = matrix[i][j];
+        const q3 = matrix[(2 * quadrantSize) - i - 1][j];
+        const q4 = matrix[(2 * quadrantSize) - i - 1][(2 * quadrantSize) - j - 1)];
+        
+        sum+= Math.max(q1, q2, q3, q4);
+      }
+    }
+  
+    return sum;
+    
+}
+
+
+
